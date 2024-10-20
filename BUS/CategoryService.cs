de8db777d8d7e11f619e64673a8d4f8529a083ac
Mày không biết tao là ai <132144119+Tavjppro215@users.noreply.github.com>
2024-10-20 23:32:01 +0700

    202410202331 DAL + BUS Category

---
+++ BUS/CategoryService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class CategoryService
+    {
+        private static CategoryService _instance;
+        public static CategoryService Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new CategoryService();
+                return _instance;
+            }
+            set => _instance = value;
+        }
+    }
+}
---
+++ DAL/DALCategory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DALCategory
+    {
+        private static DALCategory _instance;
+        public static DALCategory Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new DALCategory();
+                return _instance;
+            }
+            set => _instance = value;
+        }
+    }
+}
